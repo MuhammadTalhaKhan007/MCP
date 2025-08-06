6ba1bd83da468b8156910e8ab9160bbab37c5a2c
Muhammad Talha Khan <mtalha@kodexlabs.com>
2025-08-07 00:38:31 +0500

    Ran MCP inspector

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -12,7 +12,7 @@ docs = {
     "plan.md": "The plan outlines the steps for the project's implementation.",
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
-
+# Tool: Read document contents and return it as a string.
 @mcp.tool(
     name="read_doc_contents",
     description="Read the contents of a document and return it as a string.",
@@ -24,7 +24,22 @@ def read_document(
         raise ValueError(f"Doc with id '{doc_id}' not found.")
     
     return docs[doc_id]
-# TODO: Write a tool to edit a doc
+
+# Tool: Edit the document by replacing a string in the document content with a new string.
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the document content with a new string."
+)
+def edit_document(
+    doc_id: str=Field(description="Id of the document that will be edited."),
+    old_str: str=Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_str: str=Field(description="The new text to insert in place of the old text.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    docs[doc_id]=docs[doc_id].replace(old_str, new_str)
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
